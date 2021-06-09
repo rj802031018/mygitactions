894d2eeeeece6f595981a2ff8c2fbba0b6e78cce
rj802031018 <85315356+rj802031018@users.noreply.github.com>
2021-06-09 12:53:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,156 +1,361 @@
-import numpy as np
-import argparse
-import matplotlib.pyplot as plt
-import cv2
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import os
-import glob, random
-from playsound import playsound
+import time,datetime
+import RPi.GPIO as GPIO
+import telepot
+from telepot.loop import MessageLoop
 import requests
+led1=12
+led2=16
+led3=23
+led4=25
+led5=26
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(led1,GPIO.OUT)
+GPIO.output(led1,GPIO.LOW)
+GPIO.setup(led2,GPIO.OUT)
+GPIO.output(led2,GPIO.LOW)
+GPIO.setup(led3,GPIO.OUT)
+GPIO.output(led3,GPIO.LOW)
+GPIO.setup(led4,GPIO.OUT)
+GPIO.output(led4,GPIO.LOW)
+GPIO.setup(led5,GPIO.OUT)
+GPIO.output(led5,GPIO.LOW)
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-# command line argument
-ap = argparse.ArgumentParser()
-ap.add_argument("--mode",help="train/display")
-mode = ap.parse_args().mode
-
-# plots accuracy and loss curves
-def plot_model_history(model_history):
-    """
-    Plot Accuracy and Loss curves given the model_history
-    """
-    fig, axs = plt.subplots(1,2,figsize=(15,5))
-    # summarize history for accuracy
-    axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
-    axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
-    axs[0].set_title('Model Accuracy')
-    axs[0].set_ylabel('Accuracy')
-    axs[0].set_xlabel('Epoch')
-    axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
-    axs[0].legend(['train', 'val'], loc='best')
-    # summarize history for loss
-    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
-    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
-    axs[1].set_title('Model Loss')
-    axs[1].set_ylabel('Loss')
-    axs[1].set_xlabel('Epoch')
-    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
-    axs[1].legend(['train', 'val'], loc='best')
-    fig.savefig('plot.png')
-    plt.show()
-
-# Define data generators
-train_dir = 'data/train'
-val_dir = 'data/test'
-
-num_train = 28709
-num_val = 7178
-batch_size = 64
-num_epoch = 30
-
-train_datagen = ImageDataGenerator(rescale=1./255)
-val_datagen = ImageDataGenerator(rescale=1./255)
-
-train_generator = train_datagen.flow_from_directory(
-        train_dir,
-        target_size=(48,48),
-        batch_size=batch_size,
-        color_mode="grayscale",
-        class_mode='categorical')
-
-validation_generator = val_datagen.flow_from_directory(
-        val_dir,
-        target_size=(48,48),
-        batch_size=batch_size,
-        color_mode="grayscale",
-        class_mode='categorical')
-
-# Create the model
-model = Sequential()
-
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(7, activation='softmax'))
-
-# If you want to train the same model or try other models, go for this
-if mode == "train":
-    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
-    model_info = model.fit_generator(
-            train_generator,
-            steps_per_epoch=num_train // batch_size,
-            epochs=num_epoch,
-            validation_data=validation_generator,
-            validation_steps=num_val // batch_size)
-    plot_model_history(model_info)
-    model.save_weights('emotion_trained.h5')
-
-# emotions will be displayed on your face from the webcam feed
-elif mode == "display":
-    model.load_weights('emotion_trained.h5')
-
-    # prevents openCL usage and unnecessary logging messages
-    cv2.ocl.setUseOpenCL(False)
-
-    # dictionary which assigns each label an emotion (alphabetical order)
-    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
-    # start the webcam feed
-    cap = cv2.VideoCapture(0)
-    while True:
-        # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        if not ret:
-            break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-
-        cv2.imshow('Video', cv2.resize(frame,(800,700),interpolation = cv2.INTER_CUBIC))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-    print('Please wait we are searching best reccommendation for you')
-    import time
-    time.sleep(3)
-    print('We got it !!!')
-    file=emotion_dict[maxindex]
-    print(file)
-
-    base_url=('https://api.telegram.org/bot1873730243:AAGSufK0YhIlC8MRnhTbsjVZlAvGJoPLDS4/sendMessage?chat_id=-428255520&text='+file)
-    requests.get(base_url)
-
-    #telegram(file)
-    file_directory = glob.glob("D:\\ME\\SEM 2\\Project ED\\Detection\\music\\"+file+"\\*")
-    random_directory = random.choice(file_directory)
-    audio_file = playsound(random_directory)
-    print(random_directory)
+def action(msg):
+    chat_id=msg['chat']['id']
+    command=msg['text']
+    print('Emotion: %s' % command)
+    if 'happy' in command:
+        message="happy"
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        telegram_bot.sendMessage(chat_id,message)
+        base_url='https://api.telegram.org/bot1873730243:AAGSufK0YhI1C8MRnhTbsjVZlAvGJoPLDS4/sendMessage?chat_id=-428255520&text="Happy"'
+        requests.get(base_url)
+    if 'sad' in command:
+        message="sad"
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led2,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led3,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led4,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led4,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led3,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led2,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led2,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led3,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led4,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led4,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led3,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led2,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led2,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led3,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led4,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led4,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led3,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led2,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        telegram_bot.sendMessage(chat_id,message)
+        base_url='https://api.telegram.org/bot1873730243:AAGSufK0YhI1C8MRnhTbsjVZlAvGJoPLDS4/sendMessage?chat_id=-428255520&text="Sad"'
+        requests.get(base_url)
+    if 'angry' in command:
+        message="angry"
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.LOW)
+        telegram_bot.sendMessage(chat_id,message)
+        base_url='https://api.telegram.org/bot1873730243:AAGSufK0YhI1C8MRnhTbsjVZlAvGJoPLDS4/sendMessage?chat_id=-428255520&text="Angry"'
+        requests.get(base_url)
+    if 'disgusted' in command:
+        message="disgusted"
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.HIGH)
+        telegram_bot.sendMessage(chat_id,message)
+        base_url='https://api.telegram.org/bot1873730243:AAGSufK0YhI1C8MRnhTbsjVZlAvGJoPLDS4/sendMessage?chat_id=-428255520&text="Disgusted"'
+        requests.get(base_url)
+    if 'surprised' in command:
+        message="surprised"
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        telegram_bot.sendMessage(chat_id,message)
+        base_url='https://api.telegram.org/bot1873730243:AAGSufK0YhI1C8MRnhTbsjVZlAvGJoPLDS4/sendMessage?chat_id=-428255520&text="Surprised"'
+        requests.get(base_url)
+    if 'neutral' in command:
+        message="neutral"
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.HIGH)
+        telegram_bot.sendMessage(chat_id,message)
+        base_url='https://api.telegram.org/bot1873730243:AAGSufK0YhI1C8MRnhTbsjVZlAvGJoPLDS4/sendMessage?chat_id=-428255520&text="Neutral"'
+        requests.get(base_url)
+    if 'fearful' in command:
+        message="fearful"
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.LOW)
+        GPIO.output(led2,GPIO.LOW)
+        GPIO.output(led3,GPIO.LOW)
+        GPIO.output(led4,GPIO.LOW)
+        GPIO.output(led5,GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(led1,GPIO.HIGH)
+        GPIO.output(led2,GPIO.HIGH)
+        GPIO.output(led3,GPIO.HIGH)
+        GPIO.output(led4,GPIO.HIGH)
+        GPIO.output(led5,GPIO.HIGH)
+        telegram_bot.sendMessage(chat_id,message)
+        base_url='https://api.telegram.org/bot1873730243:AAGSufK0YhI1C8MRnhTbsjVZlAvGJoPLDS4/sendMessage?chat_id=-428255520&text="Fearful"'
+        requests.get(base_url)
+telegram_bot=telepot.Bot('1841457518:AAF-A5cYRnZ2ZMYtoAMXcy80U3X4zflUIDI')
+'''
+print(telegram_bot.getMe())
+'''
+MessageLoop(telegram_bot,action).run_as_thread()
+print('Iot module is running')
